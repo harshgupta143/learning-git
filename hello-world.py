e0620a855e7b966763f7ae4919a668bd0e5f35d4
Harsh Gupta <harshguptarampur@gmail.com>
2023-03-15 22:22:52 +0530

    subtract

--- hello-world.py
+++ hello-world.py
@@ -5,3 +5,9 @@ a=int(input("enter a number"))
 b=int(input("enter another number"))
 print(a+b)
 
+#sub two numbers
+d=int(input("enter a number"))
+e=int(input("enter another number"))
+print(d-e)
+
+#hii 
